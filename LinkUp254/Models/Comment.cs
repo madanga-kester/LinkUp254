eeab460177e2169ff8ccd365aec8f57443b69bb4
Kester Madanga <madangakester@gmail.com>
2025-12-13 19:31:14 +0300

    Posta , Comments  classes

---
+++ LinkUp254/Models/Comment.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LinkUp254.Models
+{
+    public class Comment : BaseEntity
+    {
+        [Required]
+        public int PostId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string Content { get; set; } = string.Empty;
+
+        public int? ParentCommentId { get; set; }
+
+        [ForeignKey(nameof(PostId))]
+        public Post Post { get; set; } = null!;
+
+        [ForeignKey(nameof(UserId))]
+        public Users User { get; set; } = null!;
+
+        [ForeignKey(nameof(ParentCommentId))]
+        public Comment? ParentComment { get; set; }
+
+        public ICollection<Comment> Replies { get; set; } = new List<Comment>();
+    }
+}
---
+++ LinkUp254/Models/Posta.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LinkUp254.Models
+{
+    public class Post : BaseEntity
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(3000)]
+        public string Content { get; set; } = string.Empty;
+
+        public string? Visibility { get; set; } = "Public"; // Public, Followers, Private
+
+        [ForeignKey(nameof(UserId))]
+        public Users Author { get; set; } = null!;
+
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+       // public ICollection<PostLike> Likes { get; set; } = new List<PostLike>();
+       // public ICollection<PostShare> Shares { get; set; } = new List<PostShare>();
+        public ICollection<MediaFile> MediaFiles { get; set; } = new List<MediaFile>();
+    }
+}
