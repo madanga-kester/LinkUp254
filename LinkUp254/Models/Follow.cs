b31c7ab01f9945ff4ff98a742623091ba64c17b1
Kester Madanga <madangakester@gmail.com>
2025-12-15 15:48:04 +0300

    Created Postlike and follow Classes

---
+++ LinkUp254/Models/Follow.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LinkUp254.Models
+{
+    public class Follow : BaseEntity
+    {
+        public int FollowerId { get; set; }
+        public int FollowingId { get; set; }
+
+        [ForeignKey(nameof(FollowerId))]
+        public Users Follower { get; set; } = null!;
+
+        [ForeignKey(nameof(FollowingId))]
+        public Users Following { get; set; } = null!;
+    }
+}
---
+++ LinkUp254/Models/PostLike.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LinkUp254.Models
+{
+    public class PostLike : BaseEntity
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+
+        [ForeignKey(nameof(PostId))]
+        public Post Post { get; set; } = null!;
+
+        [ForeignKey(nameof(UserId))]
+        public Users User { get; set; } = null!;
+    }
+}
