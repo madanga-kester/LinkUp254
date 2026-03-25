36c997adc88e7cbf80ca62bbd66838ee2db092bf
Kester Madanga <madangakester@gmail.com>
2026-03-25 15:18:51 +0300

    migration

---
+++ LinkUp254/Migrations/20260325120648_InitialCreation.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkUp254.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Users_UsersId",
+                table: "Events");
+
+            migrationBuilder.DropTable(
+                name: "EventAtendees");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Phone",
+                table: "Users",
+                newName: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "UsersId",
+                table: "Events",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Events_UsersId",
+                table: "Events",
+                newName: "IX_Events_UserId");
+
+            migrationBuilder.CreateTable(
+                name: "EventAttendees",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    RsvpStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventAttendees", x => new { x.EventId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_EventAttendees_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventAttendees_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventAttendees_UserId",
+                table: "EventAttendees",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Users_UserId",
+                table: "Events",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Users_UserId",
+                table: "Events");
+
+            migrationBuilder.DropTable(
+                name: "EventAttendees");
+
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "Phone");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Events",
+                newName: "UsersId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Events_UserId",
+                table: "Events",
+                newName: "IX_Events_UsersId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "EventAtendees",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    RsvpStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventAtendees", x => new { x.EventId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_EventAtendees_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventAtendees_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventAtendees_UserId",
+                table: "EventAtendees",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Users_UsersId",
+                table: "Events",
+                column: "UsersId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
